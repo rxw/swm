21fdeda750ea0ca958cd2791215315a909cd9ccc
tatosaurus <tato@uribe.com.mx>
2014-11-24 09:44:21 -0600

    first commit

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
-#define MOD		SUPER		/* modifier (CTRL, ALT, SUPER, etc.) */
-#define FOCUSCOL	0x18191A	/* focused   inner border color */
-#define UNFOCUSCOL	0x141516	/* unfocused inner border color */
+#define MOD		ALT		/* modifier (CTRL, ALT, SUPER, etc.) */
+#define FOCUSCOL	0xcc6464	/* focused   inner border color */
+#define UNFOCUSCOL	0xffffff	/* unfocused inner border color */
 #define OUTER_COLOR	0x000000	/*  outer border color */
 #define RESIZE_COLOR	0xC1C189	/* resize border color */
 #define BORDERWIDTH	4		/*  full  border width */
